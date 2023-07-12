6fbe734696a5afd2cbc5f9c408d5b9e1efb7425f
Michal Nowak <michal.nowak.geo@gmail.com>
2023-07-12 12:32:32 +0200

    first commit

---
+++ src/main/java/pl/michalnowak/tirecenter/TirecenterApplication.java
@@ -0,0 +1,13 @@
+package pl.michalnowak.tirecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TirecenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TirecenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/michalnowak/tirecenter/TirecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalnowak.tirecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TirecenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
